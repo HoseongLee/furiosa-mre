b6b6cec1e51743da78f5f6dbb57389199f47eb01
HoseongLee <leehs.git@gmail.com>
2026-07-04 15:31:00 +0900

    Updated to 0.4.0

--- src/dm_to_dm.rs
+++ src/dm_to_dm.rs
@@ -16,5 +16,5 @@ error: could not compile `mre` (lib) due to 1 previous error
 #[device(chip = 1)]
 pub fn forward(ctx: &mut Context, q: &HbmTensor<bf16, Chip, m![S, Q]>) {
     let q: DmTensor<bf16, Chip, Cluster, m![S # 256], m![Q]> = q.to_dm(&mut ctx.tdma);
-    let q: DmTensor<bf16, Chip, Cluster, m![S # 256], m![Q]> = q.to_dm(&mut ctx.tdma, 0);
+    let q: DmTensor<bf16, Chip, Cluster, m![S # 256], m![Q]> = q.to_dm(&mut ctx.tdma);
 }
